0b7c3ce563fab8a0eeb3bfe3e31cfa3330d9cfad
Maxx Sanner <maxx.sanner@qsc.com>
2022-03-10 18:21:47 -0500

    fixed bug in runtime that caused pin pad to append incorrect digits

--- runtime.lua
+++ runtime.lua
@@ -15,7 +15,7 @@ local AddPinPin = Controls.AddPinPin
 local AddPinSubmit = Controls.AddPinSubmit
 local Status = Controls.Status
 
-local Digits = {"1","2","3","4","5","6","7","8","9","0","*","#"}
+local Digits = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"}
 local PinData = {}
 local UCIData = {}
 PollDataTimer = Timer.New()
