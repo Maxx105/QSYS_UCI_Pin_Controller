4f9ad1c3ff9aec38a447579b3fdb1b8fa5e615d9
Maxx Sanner <maxx.sanner@qsc.com>
2022-03-15 12:49:46 -0400

    updated version number in info.lua

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   Name = "My Plugins~UCI Pin Controller",
-  Version = "1.1",
-  BuildVersion = "0.0.0.0",
+  Version = "1.2",
+  BuildVersion = "1.2.0.0",
   Id = "297d2ac8-8a49-40f7-b608-7f042a185039",
   Author = "Maxx Sanner",
   Description = "A plugin that provides a custom pinpad as well as the ability to dynamically and easily assign a pin to a UCI. "  
